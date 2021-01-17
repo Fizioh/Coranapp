dde26f54f8c919400bb03e40a07f974e2f59e77f
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2021-01-17 11:43:27 +0100

    data calls

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,19 +3,47 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({data : {confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed) {
+        return 'Loading...';
+    }
 
 
     return (
       <div className={styles.container}>
           <Grid container spacing={3} justify="center">
+
+               {/*------- CARD #1 -------*/}
             <Grid item component={Card}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                    <Typography  varaint="h5">REAL DATA</Typography>
+                    <Typography  varaint="h5">{confirmed.value}</Typography>
                     <Typography  color="textSecondary">REAL DATE</Typography>
                     <Typography  variant="body2">Nombre de cas actifs de Covid-19</Typography>
+
+                </CardContent>
+                </Grid>
+
+                    {/*------- CARD #2 -------*/}
+
+                <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography  varaint="h5">{recovered.value}</Typography>
+                    <Typography  color="textSecondary">REAL DATE</Typography>
+                    <Typography  variant="body2">Nombre de morts de Covid-19</Typography>
+
+                </CardContent>
+                </Grid>
+
+                {/*------- CARD #3 -------*/}
+
+                <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography  varaint="h5">{deaths.value}</Typography>
+                    <Typography  color="textSecondary">REAL DATE</Typography>
+                    <Typography  variant="body2">Nombre de guérisons de Covid-19</Typography>
 
                 </CardContent>
                 </Grid>
