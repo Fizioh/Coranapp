a71146ada9cb907d42159a2702387f905b21d467
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2021-01-17 12:14:25 +0100

    add styles/repsonsive

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import cx from'classnames';
 
 import styles from './Cards.module.css';
 
@@ -15,18 +16,13 @@ const Cards = ({data : {confirmed, recovered, deaths, lastUpdate}}) => {
           <Grid container spacing={3} justify="center">
 
                {/*------- CARD #1 -------*/}
-            <Grid item component={Card}>
+            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                 <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography color="textSecondary" gutterBottom>Cas</Typography>
                     <Typography  varaint="h5">
-                        <CountUp
-                        start={0}
-                        end={confirmed.value}
-                        duration={2.8}
-                        separator=","
-                        />
+                    <CountUp start={0} end={confirmed.value} duration={2.3} separator=","/>
                         </Typography>
-                    <Typography  color="textSecondary">REAL DATE</Typography>
+                    <Typography  color="textSecondary">{new Date( lastUpdate).toLocaleDateString()}</Typography>
                     <Typography  variant="body2">Nombre de cas actifs de Covid-19</Typography>
 
                 </CardContent>
@@ -34,17 +30,13 @@ const Cards = ({data : {confirmed, recovered, deaths, lastUpdate}}) => {
 
                     {/*------- CARD #2 -------*/}
 
-                <Grid item component={Card}>
+                    <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                 <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography color="textSecondary" gutterBottom>Guérisons</Typography>
                     <Typography  varaint="h5">
-                    <CountUp
-                        start={0}
-                        end={deaths.value}
-                        duration={2.8}
-                        separator=","
-                        /></Typography>
-                    <Typography  color="textSecondary">REAL DATE</Typography>
+                    <CountUp start={0} end={recovered.value} duration={2.3} separator=","/>
+                        </Typography>
+                    <Typography  color="textSecondary">{new Date( lastUpdate).toLocaleDateString()}</Typography>
                     <Typography  variant="body2">Nombre de morts de Covid-19</Typography>
 
                 </CardContent>
@@ -52,18 +44,13 @@ const Cards = ({data : {confirmed, recovered, deaths, lastUpdate}}) => {
 
                 {/*------- CARD #3 -------*/}
 
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                 <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography color="textSecondary" gutterBottom>Décès</Typography>
                     <Typography  varaint="h5">
-                    <CountUp
-                        start={0}
-                        end={recovered.value}
-                        duration={2.8}
-                        separator=","
-                        /></Typography>
-                    </Typography>
-                    <Typography  color="textSecondary">REAL DATE</Typography>
+                    <CountUp start={0} end={deaths.value} duration={2.3} separator=","/>
+                        </Typography>
+                    <Typography  color="textSecondary">{new Date( lastUpdate).toLocaleDateString()}</Typography>
                     <Typography  variant="body2">Nombre de guérisons de Covid-19</Typography>
 
                 </CardContent>
