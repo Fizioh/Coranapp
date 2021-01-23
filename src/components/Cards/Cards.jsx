cca6b3454ab13c75cdb45600ce6774396d8105f0
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2021-01-24 00:52:41 +0100

    Add files via upload

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,63 +1,41 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
-import CountUp from 'react-countup';
-import cx from'classnames';
-
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
 import styles from './Cards.module.css';
 
-const Cards = ({data : {confirmed, recovered, deaths, lastUpdate}}) => {
-    if(!confirmed) {
-        return 'Loading...';
-    }
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
 
+  return (
+    <div className={styles.container}>
+        <Typography gutterBottom variant="h4" component="h2">Statistiques Covid-19 par pays</Typography>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infectés"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Nombre de cas actifs de COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Guérisons"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Nombre de guérisons de COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Morts"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Nombre de morts du COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
 
-    return (
-      <div className={styles.container}>
-          <Grid container spacing={3} justify="center">
-
-               {/*------- CARD #1 -------*/}
-            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Cas</Typography>
-                    <Typography  varaint="h5">
-                    <CountUp start={0} end={confirmed.value} duration={2.3} separator=","/>
-                        </Typography>
-                    <Typography  color="textSecondary">{new Date( lastUpdate).toLocaleDateString()}</Typography>
-                    <Typography  variant="body2">Nombre de cas actifs de Covid-19</Typography>
-
-                </CardContent>
-                </Grid>
-
-                    {/*------- CARD #2 -------*/}
-
-                    <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Guérisons</Typography>
-                    <Typography  varaint="h5">
-                    <CountUp start={0} end={recovered.value} duration={2.3} separator=","/>
-                        </Typography>
-                    <Typography  color="textSecondary">{new Date( lastUpdate).toLocaleDateString()}</Typography>
-                    <Typography  variant="body2">Nombre de morts de Covid-19</Typography>
-
-                </CardContent>
-                </Grid>
-
-                {/*------- CARD #3 -------*/}
-
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Décès</Typography>
-                    <Typography  varaint="h5">
-                    <CountUp start={0} end={deaths.value} duration={2.3} separator=","/>
-                        </Typography>
-                    <Typography  color="textSecondary">{new Date( lastUpdate).toLocaleDateString()}</Typography>
-                    <Typography  variant="body2">Nombre de guérisons de Covid-19</Typography>
-
-                </CardContent>
-                </Grid>
-          </Grid>
-      </div>
-    )
-}
-
-export default Cards;
+export default Info;
